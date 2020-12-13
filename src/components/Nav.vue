aee521c86fa4f7009991c73160ee9e7b9986c7e4
唐宜锦 <532677540@qq.com>
2020-12-13 16:51:18 +0800

    引入svg并配置

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,28 @@
 <template>
       <nav>
-        <router-link to="/money">记账</router-link> |
-        <router-link to="/labels">标签</router-link>|
-        <router-link to="/statistics">统计</router-link>
+        <router-link to="/money">
+          <svg>
+            <use xlink:href="#money"></use>
+          </svg>
+          记账</router-link>
+        <router-link to="/labels">
+          <svg>
+            <use xlink:href="#label"></use>
+          </svg>标签</router-link>
+        <router-link to="/statistics">
+          <svg>
+            <use xlink:href="#statistics"></use>
+          </svg>统计</router-link>
       </nav>
 </template>
 
 <script lang="ts">
+const importAll = (requireContext )=> requireContext.keys().forEach(requireContext);
+try{
+  importAll( require.context('../assets/icons', true, /\.svg$/))
+}catch(err){
+  console.log(err)
+}
 export default {
 name: "Navs"
 }
