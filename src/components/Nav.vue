91a0c87b30f1ad9939a2d24bc237fc15a73eb435
唐宜锦 <532677540@qq.com>
2020-12-16 18:04:56 +0800

    完成money页面样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,42 +1,50 @@
 <template>
-      <nav>
-        <router-link to="/money" class="item" active-class="selected" >
-          <Icon name="money" />
-          记账</router-link>
-        <router-link to="/labels" class="item" active-class="selected">
-          <Icon name="label" />标签</router-link>
-        <router-link to="/statistics" class="item" active-class="selected">
-          <Icon name="statistics" />统计</router-link>
-      </nav>
+  <nav>
+    <router-link to="/money" class="item" active-class="selected">
+      <Icon name="money"/>
+      记账
+    </router-link>
+    <router-link to="/labels" class="item" active-class="selected">
+      <Icon name="label"/>
+      标签
+    </router-link>
+    <router-link to="/statistics" class="item" active-class="selected">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
 
 export default {
-name: "Navs"
-}
+  name: 'Navs'
+};
 </script>
 
 <style lang="scss" scoped>
-nav{
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+nav {
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   display: flex;
-//flex-direction: row;
+  //flex-direction: row;
   font-size: 12px;
-  .item{
-    flex:1;    //width:33.3333%;
+
+  > .item {
+    width: 33.3333%;
     padding: 2px 0;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    .icon{
+
+    .icon {
       width: 32px;
       height: 32px;
     }
 
   }
-  .item.selected{
+
+  .item.selected {
     color: red;
   }
 }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,80 @@
 <template>
-<div>  <Layout>
-  money
-</Layout></div>
+  <div>
+    <Layout classPrefix="layout">
+      <div class="numberPad">
+        <div class="numberInput">100</div>
+        <div class="buttons">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>0</button>
+          <button>.</button>
+          <button>ok</button>
+        </div>
+
+      </div>
+      <ul class="types">
+        <li class="selected">收入</li>
+        <li>支出</li>
+      </ul>
+      <label class="notes">
+        <span>备注</span>
+        <input type="text" placeholder="在这里输入备注">
+      </label>
+      <div class="tags">
+        <div class="new">
+          <button>新增标签</button>
+        </div>
+        <ul class="current">
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+        </ul>
+
+      </div>
 
 
 
+    </Layout>
+  </div>
 </template>
 
 <script lang="ts">
@@ -12,8 +82,159 @@ export default {
   name: 'Money'
 };
 </script>
-
+<style  lang="scss">
+.layout-content{
+  display: flex;
+  flex-direction: column-reverse;
+}
+</style>
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+
+.tags {
+  font-size: 14px;
+  padding: 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column-reverse;
+  > .current {
+    display: flex;
+    flex-wrap: wrap;
+    li {
+      $bg: #d9d9d9;
+      background: $bg;
+      height: 24px;
+      border-radius: 12px;
+      margin-right: 12px;
+      padding: 0 16px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 4px;
+      &.selected {
+        background: darken($bg, 50%);
+        color: white;
+      }
+    }
+  }
+
+  .new {
+    padding-top: 16px;
+
+    button {
+      background: transparent;
+      border: none;
+      color: #999;
+      padding: 0 4px;
+      border-bottom: 1px solid;
+    }
+  }
+}
+
+.notes {
+  font-size: 14px;
+  background: #f5f5f5;
+  display: flex;
+  align-items: center;
+  padding-left: 16px;
+
+  input {
+    padding-left: 16px;
+    height: 64px;
+    flex-grow: 1;
+    color: #ccc;
+    border: none;
+    background: transparent;
+  }
+}
+
+.types {
+  background: #c4c4c4;
+  display: flex;
+
+  > li {
+    padding: 14px 0;
+    font-size: 24px;
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+
+    &.selected::after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: #333333;
+    }
+  }
+}
+
+.numberPad {
+  .numberInput {
+    @extend %innerShadow;
+    text-align: right;
+    font-size: 36px;
+    font-family: Consolas, monospace;
+    padding: 9px 16px;
+
+  }
+
+  .buttons {
+    position: relative;
+
+    button {
+      width: 25%;
+      height: 64px;
+      border: none;
+      background: transparent;
+    }
+
+    $bg: #f2f2f2;;
+
+    :nth-child(1) {
+      background: $bg;
+    }
+
+    :nth-child(2), :nth-child(5) {
+      background: darken($bg, 3%);
+    }
+
+    :nth-child(3), :nth-child(6), :nth-child(9) {
+      background: darken($bg, 9%);
+    }
+
+    :nth-child(4), :nth-child(7), :nth-child(10) {
+      background: darken($bg, 12%);
+    }
+
+    :nth-child(8), :nth-child(11) {
+      background: darken($bg, 15%);
+    }
+
+    :nth-child(12) {
+      background: darken($bg, 15%);
+      width: 50%;
+    }
+
+    :nth-child(13) {
+      background: darken($bg, 18%);
+    }
+
+    :nth-child(14) {
+      height: 128px;
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      background: darken($bg, 21%);
+    }
+
+  }
+}
+
 .nav-wrapper {
   display: flex;
   flex-direction: column;
