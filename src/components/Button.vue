39d26df21e5f33cb730f95408a1d52a92c7bacfd
唐宜锦 <532677540@qq.com>
2020-12-24 17:32:45 +0800

    改写为ts格式

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -5,8 +5,12 @@
 </template>
 
 <script lang="ts">
-export default {
-name: "Button"
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Button extends Vue{
+
 }
 </script>
 
