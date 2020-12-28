6200dbd57802e49d4abb84619a489ca9f83448aa
唐宜锦 <532677540@qq.com>
2020-12-28 16:09:20 +0800

    将类型别名整合到一起

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -5,3 +5,15 @@ type RecordItem = {
   amount: number;
   createAt: Date;
 }
+type Tag = {
+  id: string;
+  name: string;
+}
+type TagListModel = {
+  data: Tag[];
+  save: () => void;
+  fetch: () => Tag[];
+  create: (name: string) => 'success' | 'duplicated';
+  remove: (tag: Tag) => boolean;
+  update: (id: string,name: string) => 'success' | 'not found';
+}
