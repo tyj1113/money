8ff37c02de8e52a9e5369a72bd79a24794cd1c8a
唐宜锦 <532677540@qq.com>
2021-01-10 19:31:53 +0800

    改造数据植入chart组件

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <Layout>
+
       <Types :value.sync="type" class-prefix="type"/>
+      <div class="chartWrapper" ref="chartWrapper">
+        <Chart :option="chartOption" class="chart"/>
+      </div>
+
       <ol v-if="groupList.length!==0">
         <li v-for="(group,index) in groupList" :key="index">
           <h3 class="title">{{ dateFormat(group.title) }}<span>￥{{ group.total }}</span></h3>
@@ -29,16 +34,82 @@ import {Component} from 'vue-property-decorator';
 import Types from '@/components/Money/Types.vue';
 import clone from '@/lib/clone';
 import dayjs from 'dayjs';
+import Chart from '@/components/Chart.vue';
 
 
 @Component({
-  components: {Types}
+  components: {Chart, Types}
 })
 export default class Statistics extends Vue {
   type = '-';
 
   beforeCreate() {
     this.$store.commit('fetchRecords');
+  }
+
+  mounted() {
+    const div = this.$refs.chartWrapper as HTMLDivElement;
+    div.scrollLeft = div.scrollWidth;
+  }
+
+  get axisData() {
+    const today = new Date();
+    const array: { date: string; total: number } [] = [];
+    for (let i = 0; i < 30; i++) {
+      array.unshift({date: dayjs(today).subtract(i, 'day').format('YYYY-MM-DD'), total: 0});
+      const item = this.groupList.find(item => item.title === array[0].date);
+      if (item) {
+        array[0].total = item.total;
+      }
+    }
+    return array;
+  }
+
+  get chartOption() {
+    const yData = this.axisData.map(item => item.date);
+    const xData = this.axisData.map(item => item.total);
+    return {
+      tooltip: {
+        show: true,
+        triggerOn: 'click',
+        position: 'top',
+        formatter: '{c}'
+      },
+      xAxis: {
+        type: 'category',
+        data: yData,
+        axisTick: {
+          show: false
+        },
+        axisLine: {
+          lineStyle: {
+            color: '#333'
+          }
+        },
+        axisLabel: {
+          formatter: function (value: string) {
+            return value.substr(5);
+          }
+        }
+      },
+      yAxis: {show: false},
+      series: [{
+        type: 'line',
+        symbolSize: 10,
+        symbol: 'circle',
+        itemStyle: {
+          borderWidth: 1,
+          color: '#666',
+          borderColor: '#666'
+        },
+        data: xData
+      }],
+      grid: {
+        left: 0,
+        right: 0
+      }
+
+    };
   }
 
   get recordList() {
@@ -48,7 +119,7 @@ export default class Statistics extends Vue {
   get groupList() {
     const {recordList} = this;
     const newList = clone(recordList).filter(r => r.type === this.type).sort((a, b) => dayjs(b.createAt).valueOf() - dayjs(a.createAt).valueOf());
-    if(newList.length===0) return []
+    if (newList.length === 0) return [];
     type Result = { title: string; total: number; items: RecordItem[] }[]
     const result: Result = [
       {
@@ -132,9 +203,18 @@ export default class Statistics extends Vue {
     color: #999;
   }
 }
-.noRecord{
+
+.noRecord {
   text-align: center;
   margin: 50px auto 0;
+}
+
+.chart {
+  width: 430%;
+
+  &Wrapper {
+    overflow: auto;
+  }
 }
 </style>
 
