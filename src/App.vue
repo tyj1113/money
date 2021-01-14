f8294b3b622625b30ea1a3024f073c03a5662c6e
tyj1113 <1991383016@qq.com>
2021-01-14 17:26:22 +0800

    改造为ts 解决打包报错

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,13 @@
     <router-view/>
   </div>
 </template>
-<script>
+<script lang="ts">
+  import Vue from "vue";
+  import {Component} from "vue-property-decorator";
+@Component
+  export default class App extends Vue{
 
+  }
 </script>
 <style lang="scss">
 @import "~@/assets/style/reset.scss";
